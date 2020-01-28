bc726961a9c4b8b053011fd3b8cfd215d673df13
Charles Pelton <charliejp0311@gmail.com>
2020-01-28 17:44:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,15 +6,15 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => []      
+      :heroine_friends => []
       }
     }
 
-  
+
 end
